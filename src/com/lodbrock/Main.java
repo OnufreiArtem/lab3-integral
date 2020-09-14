af0ef88da67d4aff30c09edce0f54874ad9fc1c6
OnufreiArtem <2hilderoy2@gmail.com>
2020-09-14 20:08:00 +0300

    added integral logic

--- src/com/lodbrock/Main.java
+++ src/com/lodbrock/Main.java
@@ -1,8 +1,33 @@
 package com.lodbrock;
 
+import com.sun.xml.internal.ws.client.ClientTransportException;
+
+import java.util.function.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Function<Double, Double> function = Math::sin;
+
+        double start = 0;
+	    double end   = Math.PI;
+
+	    double lineHeight = 0.1;
+
+	    int accuracy = 100;
+
+	    double range = end - start;
+	    double value = range / accuracy;
+
+	    double integral = 0;
+
+	    for(int step = 0; step < accuracy; step++){
+            double area = value;
+            double functionHeight = Math.abs(function.apply(step*value));
+            area *= Math.min(functionHeight, lineHeight);
+	        integral += area;
+        }
+        System.out.println("The integral is of current function is " + integral);
+
     }
+
 }
