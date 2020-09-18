ee2f8ad3671831f7daa6f7bd47b302cefabecd9d
OnufreiArtem <2hilderoy2@gmail.com>
2020-09-18 10:43:42 +0300

    Resolved bug with line position

--- src/com/lodbrock/Main.java
+++ src/com/lodbrock/Main.java
@@ -5,12 +5,12 @@ import java.util.function.*;
 public class Main {
 
     public static void main(String[] args) {
-	    Function<Double, Double> function = Math::sin;
+	    Function<Double, Double> function = x -> -Math.sin(x);
 
         double start = 0;
 	    double end   = Math.PI;
 
-	    double lineHeight = 0.1;
+	    double lineHeight = 0.5;
 
 	    int accuracy = 100;
 
@@ -21,10 +21,19 @@ public class Main {
 
 	    for(int step = 0; step < accuracy; step++){
             double area = value;
-            double functionHeight = Math.abs(function.apply(step*value));
-            area *= Math.min(functionHeight, lineHeight);
-	        integral += area;
+			double functionResult = function.apply(step*value);
+
+			if(lineHeight > 0)
+				area *= functionResult < 0 ? Math.abs(functionResult) : Math.min(functionResult, lineHeight);
+	        else
+			{
+				area *= functionResult < 0 ? Math.max(functionResult, lineHeight) : functionResult;
+				area = Math.abs(area);
+			}
+
+			integral += area;
         }
+
         System.out.println("The integral is of current function is " + integral);
 
     }
